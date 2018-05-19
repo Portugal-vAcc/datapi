39a5e545aece96ac50fbfe3f856b793b4c8e2c3e
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-05-19 17:45:06 +0100

    Binding to 0 host to work on Heroku

--- run.py
+++ run.py
@@ -27,4 +27,4 @@ port = int(os.environ.get('PORT', 5000))
 debug = port == 5000
 
 if __name__ == '__main__':
-    app.run(port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
